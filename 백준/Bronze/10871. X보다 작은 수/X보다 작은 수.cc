b47971274506291848e481951e3720b20794e13b
Young Woo Noh <47950172+bluearrow1029@users.noreply.github.com>
2024-02-03 00:07:22 +0900

    [Bronze V] Title: X보다 작은 수, Time: 416 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, X;
+	cin >> N >> X;
+
+	int A;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A;
+
+		if (A < X)
+			cout << A << " ";
+	}
+}
