1ef8ae886f6932a4322e270e237483551a272a4e
Young Woo Noh <47950172+bluearrow1029@users.noreply.github.com>
2024-03-16 20:29:32 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int cnt = 0;
+
+int recursion(const char* s, int l, int r)
+{
+	cnt++;
+	if (l >= r)
+	{
+		return 1;
+	}
+	else if (s[l] != s[r])
+	{
+		return 0;
+	}
+	else
+	{
+		return recursion(s, l + 1, r - 1);
+	}
+}
+
+int isPalindrome(const char *s)
+{
+	return recursion(s, 0, strlen(s) - 1);
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int T;
+	cin >> T;
+
+	string s;
+	for (int i = 0; i < T; i++)
+	{
+		cnt = 0;
+		cin >> s;
+		cout << isPalindrome(s.c_str()) << ' ' << cnt <<'\n';
+	}
+	return 0;
+}
