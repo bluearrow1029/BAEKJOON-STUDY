899244c76b3741bff23b8bb7ddaeb656e929e890
Young Woo Noh <47950172+bluearrow1029@users.noreply.github.com>
2024-03-18 18:43:10 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 204 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int f[41] = { 0 };
+int count1, count2 = 0;
+
+int fib(int n)
+{
+	if (n == 1 || n == 2)
+	{
+		count1 += 1;
+		return 1;
+	}
+	else
+	{
+		return (fib(n - 1) + fib(n - 2));
+	}
+}
+
+int fibonacci(int n)
+{
+	f[1], f[2] = 1;
+
+	for (int i = 3; i <= n; i++)
+	{
+		f[i] = f[i - 1] + f[i - 2];
+		count2 += 1;
+	}
+
+	return f[n];
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	fib(n);
+	fibonacci(n);
+
+	cout << count1 << ' ' << count2;
+	return 0;
+}
