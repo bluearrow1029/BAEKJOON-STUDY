d5949a33f780e89df8cf902599b84d9a80e29914
Young Woo Noh <47950172+bluearrow1029@users.noreply.github.com>
2024-03-25 17:56:48 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector<int> v(N + 1);
+	v[0] = 0;
+	v[1] = 0;
+	for (int i = 2; i <= N; i++)
+	{
+		v[i] = v[i - 1] + 1;
+		if (i % 3 == 0)
+		{
+			v[i] = min(v[i], v[i / 3] + 1);
+		}
+		if (i % 2 == 0)
+		{
+			v[i] = min(v[i], v[i / 2] + 1);
+		}
+	}
+	cout << v[N];
+	return 0;
+}
